6da0ac154f2a764f45149fade48d08e0176177a6
GitHub Action <action@github.com>
2024-11-14 00:41:50 +0000

    Template cleanup

--- src/main/kotlin/com/github/medimohammadise/sonaraiplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/medimohammadise/sonaraiplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.medimohammadise.sonaraiplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.medimohammadise.sonaraiplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
